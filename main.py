bf1c3d053411bf00a5fe03543d67a750b9ee5810
RockSakong <fax369@hanmail.net>
2018-06-06 16:38:16 +0900

    .

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ input_drama = raw_input('drama name : ')
 result_txt = input_drama + '.txt'
 
 result_file = open(result_txt, 'w')
-model_file =  '../models/korea_221.pth.tar'
-label_txt = '../labels/221_categories.txt'
+model_file =  ''#'../models/korea_221.pth.tar'
+label_txt = ''#'../labels/221_categories.txt'
 image_path = os.path.join('drama_image', input_drama)
 
 detect(model_file, label_txt, image_path, result_file)
